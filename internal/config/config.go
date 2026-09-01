d80ba48aa48f7eea2ca77b16db00ac83e62615ca
nhassl3 <matvey.kvasov.05@mail.ru>
2026-09-01 12:01:00 +0300

    feat: add new structure in config
    Implemented cookie struct in token struct: name, domain, same_site, secure fields in new structure;
    Also update initialize of the config in go

--- internal/config/config.go
+++ internal/config/config.go
@@ -61,8 +61,17 @@ type RedisTTL struct {
 
 type Token struct {
 	PasetoKeyHex string        `yaml:"paseto_key_hex"`
+	Cookie       Cookie        `yaml:"cookie"`
 	AccessTTL    time.Duration `yaml:"access_ttl" env-default:"15m"`
 	RefreshTTL   time.Duration `yaml:"refresh_ttl" env-default:"168h"`
+}
+
+type Cookie struct {
+	Name     string `yaml:"name" env-default:"refresh_token"`
+	Domain   string `yaml:"domain" env-default:""`
+	Path     string `yaml:"path" env-default:"/"`
+	Secure   bool   `yaml:"secure" env-default:"false"`
+	SameSite string `yaml:"same_site" env-default:"lax"`
 }
 
 type SMTP struct {
@@ -117,6 +126,11 @@ func Load(configFile, envFile string) (*Config, error) {
 	yv.SetDefault("log.level", "info")
 	yv.SetDefault("log.add_caller", true)
 	yv.SetDefault("log.stacktrace", "error")
+	yv.SetDefault("token.cookie.name", "refresh_token")
+	yv.SetDefault("token.cookie.domain", "")
+	yv.SetDefault("token.cookie.path", "/")
+	yv.SetDefault("token.cookie.secure", false)
+	yv.SetDefault("token.cookie.same_site", "lax")
 
 	if err := yv.ReadInConfig(); err != nil {
 		return nil, fmt.Errorf("config: read yaml %q: %w", configFile, err)
@@ -159,6 +173,11 @@ func Load(configFile, envFile string) (*Config, error) {
 	cfg.Token.PasetoKeyHex = ev.GetString("PASETO_KEY")
 	cfg.Token.AccessTTL = yv.GetDuration("token.access_ttl")
 	cfg.Token.RefreshTTL = yv.GetDuration("token.refresh_ttl")
+	cfg.Token.Cookie.Name = yv.GetString("token.cookie.name")
+	cfg.Token.Cookie.Domain = yv.GetString("token.cookie.domain")
+	cfg.Token.Cookie.Path = yv.GetString("token.cookie.path")
+	cfg.Token.Cookie.Secure = yv.GetBool("token.cookie.secure")
+	cfg.Token.Cookie.SameSite = yv.GetString("token.cookie.same_site")
 
 	cfg.SMTP.Host = yv.GetString("smtp.host")
 	cfg.SMTP.Port = yv.GetInt("smtp.port")
