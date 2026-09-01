f6652ac8050536d72ff71960ef2b23438d2785c9
nhassl3 <matvey.kvasov.05@mail.ru>
2026-09-01 12:03:00 +0300

    feat: added new method to token maker interface

--- pkg/auth/token_manager.go
+++ pkg/auth/token_manager.go
@@ -4,4 +4,5 @@ type TokenManager interface {
 	CreateToken(username, uid, role string) (string, error)
 	CreateRefreshToken(username, uid, role string) (string, *Payload, error)
 	VerifyToken(token string) (*Payload, error)
+	GetTTL() int
 }
