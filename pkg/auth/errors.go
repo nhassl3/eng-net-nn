e64be81673f1492417900c873bb379ebb16b0d41
nhassl3 <matvey.kvasov.05@mail.ru>
2026-09-01 12:05:00 +0300

    remove: removed unused function IsAny()

--- pkg/auth/errors.go
+++ pkg/auth/errors.go
@@ -7,13 +7,3 @@ var (
 	ErrInvalidToken = errors.New("token is invalid")
 	ErrTokenRevoked = errors.New("token has been revoked")
 )
-
-// IsAny checks if error given in args in token realization errors slice
-func IsAny(err error) bool {
-	for _, t := range []error{ErrExpiredToken, ErrInvalidToken, ErrTokenRevoked} {
-		if errors.Is(err, t) {
-			return true
-		}
-	}
-	return false
-}
