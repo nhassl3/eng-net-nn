35c7a31f2e1fd4b9bd0e78ec13b769cfdf7a8cf8
nhassl3 <matvey.kvasov.05@mail.ru>
2026-09-04 11:41:26 +0300

    fix: added check on null for uuid and varchar types
    If two paramters is null -> 400 bad request

--- db/query/vacancies.sql
+++ db/query/vacancies.sql
@@ -4,20 +4,25 @@ SELECT * FROM vacancy_with_jd ORDER BY created_at DESC, id LIMIT $1 OFFSET $2;
 -- name: GetVacancy :one
 SELECT * FROM vacancy_with_jd WHERE
                             (sqlc.narg('id')::uuid IS NULL OR id=sqlc.narg('id')::uuid)
-                            AND (sqlc.narg('name')::varchar IS NULL OR name=sqlc.narg('name')::varchar);
+                            AND (sqlc.narg('name')::varchar IS NULL OR name=sqlc.narg('name')::varchar)
+                            AND (sqlc.narg('id')::uuid IS NOT NULL OR sqlc.narg('name')::varchar IS NOT NULL) LIMIT 1;
 
 -- name: CreateVacancy :one
 INSERT INTO vacancies (jd, name, description, required_exp, pay_day, skills) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;
 
 -- name: UpdateVacancy :one
-UPDATE vacancies SET jd=$1, name=sqlc.arg('new_name')::varchar, description=$2, required_exp=$3, pay_day=$4, skills=$5, updated_at=now()  WHERE
-    (sqlc.narg('id')::uuid IS NULL OR id=sqlc.narg('id')::uuid)
-                                                                                                                 AND(sqlc.narg('name')::varchar IS NULL OR name=sqlc.narg('name')::varchar) RETURNING *;
+UPDATE vacancies SET jd=$1, name=sqlc.arg('new_name')::varchar, description=$2, required_exp=$3, pay_day=$4, skills=$5, updated_at=now()
+                 WHERE
+                    (sqlc.narg('id')::uuid IS NULL OR id=sqlc.narg('id')::uuid)
+                    AND (sqlc.narg('name')::varchar IS NULL OR name=sqlc.narg('name')::varchar)
+                    AND (sqlc.narg('id')::uuid IS NOT NULL OR sqlc.narg('name')::varchar IS NOT NULL)
+                    RETURNING *;
 
 -- name: RemoveVacancy :exec
 DELETE FROM vacancies WHERE
                           (sqlc.narg('id')::uuid IS NULL OR id=sqlc.narg('id')::uuid)
-                        AND(sqlc.narg('name')::varchar IS NULL OR name=sqlc.narg('name')::varchar);
+                        AND (sqlc.narg('name')::varchar IS NULL OR name=sqlc.narg('name')::varchar)
+                        AND (sqlc.narg('id')::uuid IS NOT NULL OR sqlc.narg('name')::varchar IS NOT NULL);
 
 -- name: GetJDs :many
 SELECT * FROM job_directions ORDER BY id LIMIT $1 OFFSET $2;
