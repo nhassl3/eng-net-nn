fb7ef0342e15c13ed4abbe00495a632ee96ae8e2
nhassl3 <matvey.kvasov.05@mail.ru>
2026-09-11 12:00:00 +0300

    refactor: added new cities; improved string normalizing

--- internal/transport/gin-http/validator/validator.go
+++ internal/transport/gin-http/validator/validator.go
@@ -2,18 +2,19 @@ package validator
 
 import (
 	"slices"
+	"strings"
 
 	"github.com/go-playground/validator/v10"
 )
 
 var (
-	cities = []string{"дзержинск", "кстово", "красные баки", "нижний новгород"}
+	cities = []string{"казань", "челябинск", "нижний новгород"}
 )
 
 var (
 	ValidCity validator.Func = func(fl validator.FieldLevel) bool {
 		if city, ok := fl.Field().Interface().(string); ok {
-			return slices.Contains(cities, city)
+			return slices.Contains(cities, strings.TrimSpace(strings.ToLower(city)))
 		}
 		return false
 	}
