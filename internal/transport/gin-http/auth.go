91f7506467cbd79c75ff5266c9e8d9751a17bb33
nhassl3 <matvey.kvasov.05@mail.ru>
2026-09-13 15:15:09 +0300

    refactor: improved performance for set same_site

--- internal/transport/gin-http/auth.go
+++ internal/transport/gin-http/auth.go
@@ -3,6 +3,7 @@ package gin_http
 import (
 	"errors"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/nhassl3/IpBuild-backend/internal/domain"
@@ -141,10 +142,14 @@ func (h *Handler) clearRefreshCookie(c *gin.Context) {
 }
 
 func setSameSite(c *gin.Context, sameSite string) {
-	switch sameSite {
+	switch strings.TrimSpace(strings.ToLower(sameSite)) {
 	case "lax":
 		c.SetSameSite(http.SameSiteLaxMode)
-	default:
+	case "strict":
+		c.SetSameSite(http.SameSiteStrictMode)
+	case "none":
 		c.SetSameSite(http.SameSiteNoneMode)
+	default:
+		c.SetSameSite(http.SameSiteDefaultMode)
 	}
 }
