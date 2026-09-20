e9dc7a8dd9aed46dfe100f89d9212a0fb9aa9167
nhassl3 <matvey.kvasov.05@mail.ru>
2026-09-20 15:04:50 +0300

    feat: added new migrations
    Implemented partners talbe for the system. Also provided triggers on admins and new partners tables for creating single role users. ONLY ONE USER CAN BE admin OR partner NOT TWO. Trigger executes only when creating or updating current user with function wrotten in migration

---
+++ migrations/000017_condition_role_policy.up.sql
@@ -0,0 +1,27 @@
+CREATE OR REPLACE FUNCTION check_single_role() RETURNS trigger AS $$
+       BEGIN
+        PERFORM 1 FROM users WHERE id=NEW.user_id FOR UPDATE;
+
+        IF TG_TABLE_NAME='admins' THEN
+           IF EXISTS (SELECT 1 FROM partners WHERE user_id=NEW.user_id) THEN
+            RAISE EXCEPTION 'user % is already a partner', NEW.user_id;
+           END IF;
+        ELSIF TG_TABLE_NAME='partners' THEN
+              IF EXISTS (SELECT 1 FROM admins WHERE user_id=NEW.user_id) THEN
+                RAISE EXCEPTION 'user % is already a admin', NEW.user_id;
+              END IF;
+        END IF;
+        RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS admins_single_role ON admins;
+DROP TRIGGER IF EXISTS partners_single_role ON partners;
+
+CREATE TRIGGER admins_single_role
+    BEFORE INSERT OR UPDATE ON admins
+    FOR EACH ROW EXECUTE FUNCTION check_single_role();
+
+CREATE TRIGGER partners_single_role
+    BEFORE INSERT OR UPDATE ON partners
+    FOR EACH ROW EXECUTE FUNCTION check_single_role();
